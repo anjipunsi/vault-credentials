dce525e9849f9946029214943a7bc7d26e55c682
Anjali punsi <108992453+anjipunsi@users.noreply.github.com>
2024-08-13 17:10:10 +0530

    Update fetch_credentials.sh

--- fetch_credentials.sh
+++ fetch_credentials.sh
@@ -1,1 +1,13 @@
+#!/bin/sh
 
+# Set Vault address and token
+VAULT_ADDR=http://0.0.0.0:8200
+VAULT_TOKEN=hvs.kyNrq4xnGRQj5pnm2tqsovH3
+
+# Fetch credentials from Vault
+username=$(vault kv get -field=username secret/data/myapp)
+password=$(vault kv get -field=password secret/data/myapp)
+
+# Print credentials
+echo "Username: $username"
+echo "Password: $password"
